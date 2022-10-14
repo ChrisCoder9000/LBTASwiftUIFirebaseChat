a31efdbfd794d007b2e6f59e2ae43cb7503b113a
ChrisCoder9000 <alch.infoemail@gmail.com>
2022-10-14 17:57:20 +0200

    Initial Commit

---
+++ LBTASwiftUIFirebaseChat/LBTASwiftUIFirebaseChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  LBTASwiftUIFirebaseChatApp.swift
+//  LBTASwiftUIFirebaseChat
+//
+//  Created by Christian Nonis on 14/10/22.
+//
+
+import SwiftUI
+
+@main
+struct LBTASwiftUIFirebaseChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
